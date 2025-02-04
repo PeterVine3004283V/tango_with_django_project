7741c5c579a8fc805a4ccfc10cc55b74d20bf07b
Peter Vine <petervine1@icloud.com>
2025-02-04 00:17:45 +0000

    Ch5 exercises complete

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 
 from rango.models import Category, Page
-admin.site.register(Category)
-admin.site.register(Page)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'views', 'likes']
+
+    '''fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]'''
+admin.site.register(Category, CategoryAdmin)
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'url']
+
+    '''fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]'''
+
+admin.site.register(Page, PageAdmin)
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -79,15 +79,20 @@ WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3' , 
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
