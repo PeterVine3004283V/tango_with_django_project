2ba165256ded64630c92d8d8ec51aaa72549fd64
Peter Vine <petervine1@icloud.com>
2025-02-05 12:55:50 +0000

    Ch6 complete

--- rango/admin.py
+++ rango/admin.py
@@ -5,19 +5,11 @@ from rango.models import Category, Page
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'views', 'likes']
 
-    '''fieldsets = [
-        (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
-    ]'''
+    prepopulated_fields = {'slug':('name',)}
 admin.site.register(Category, CategoryAdmin)
-
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ['title', 'category', 'url']
 
-    '''fieldsets = [
-        (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
-    ]'''
 
 admin.site.register(Page, PageAdmin)
